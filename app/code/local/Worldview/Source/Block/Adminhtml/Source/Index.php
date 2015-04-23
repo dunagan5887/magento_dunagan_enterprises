18b2fc9f0b7ec18dfd6f6d3fc7add4cbbd790c62
Sean Dunagan <dunagan5887@gmail.com>
2015-04-23 12:44:47 -0500

    Adding button to retrieve articles from feeds to the sources grid

--- app/code/local/Worldview/Source/Block/Adminhtml/Source/Index.php
+++ app/code/local/Worldview/Source/Block/Adminhtml/Source/Index.php
@@ -16,4 +16,14 @@ class Worldview_Source_Block_Adminhtml_Source_Index
         // Don't want to allow for the addition of sources via admin panel at this time
         $this->_removeButton('add');
     }
+
+    public function getActionButtonsToRender()
+    {
+        $article_retrieval_process_button = array(
+            'action_url' => Mage::getModel('adminhtml/url')->getUrl('worldview_feed/process/articleRetrieval'),
+            'label' => 'Retrieve Articles From Sources'
+        );
+
+        return array('article_retrieval' => $article_retrieval_process_button);
+    }
 }
