bf4757d30d14518ac2b8f94b45df53be32217492
Sean Dunagan <dunagan5887@gmail.com>
2015-07-08 13:38:56 -0500

    Upgrading method to create a url safe string

--- app/code/local/Dunagan/Base/Helper/Url.php
+++ app/code/local/Dunagan/Base/Helper/Url.php
@@ -20,8 +20,13 @@ class Dunagan_Base_Helper_Url
 
     public function createUrlSafeString($unescaped_string)
     {
-        $escaped_string = str_replace($this->_special_url_characters, '_', $unescaped_string);
-        $escaped_string = str_replace('_', '-', $escaped_string);
+        $escaped_string = preg_replace('/[^A-Za-z0-9-]/', '-', $unescaped_string);
+        // url keys can only have 1 consecutive hyphen. Need to remove all consecutive hyphens
+        $escaped_string = preg_replace('/[-]{2,}/', '-', $escaped_string);
+        // url keys should not start or end with a hyphen
+        $escaped_string = preg_replace('/^-/', '', $escaped_string);
+        $escaped_string = preg_replace('/-$/', '', $escaped_string);
+
         $url_safe_string = strtolower($escaped_string);
 
         return $url_safe_string;
