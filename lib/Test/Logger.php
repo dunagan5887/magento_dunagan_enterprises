ac218f4455d769ee11cd6f6326b920cd5019dbf8
Sean Dunagan <dunagan5887@gmail.com>
2015-08-22 19:06:03 -0500

    Allowing for setting thread number on Test Logger

--- lib/Test/Logger.php
+++ lib/Test/Logger.php
@@ -6,6 +6,13 @@
 
 class Test_Logger
 {
+    static protected $_log_file = 'thread_0';
+
+    static public function setThreadNumber($thread_number = 0)
+    {
+        self::$_log_file = 'thread_' . $thread_number;
+    }
+
     static public function logMessage($message)
     {
         self::echoMessage($message);
