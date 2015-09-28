ad2657b91a9119376fc54ceb2b6424266b179a8b
Sean Dunagan <dunagan5887@gmail.com>
2015-09-28 14:28:39 -0500

    Abstract ProcessQueue Grid Page functionality

---
+++ app/code/local/Dunagan/ProcessQueue/Block/Adminhtml/Unique/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: Sean Dunagan
+ * Created: 9/25/15
+ */
+
+abstract class Dunagan_ProcessQueue_Block_Adminhtml_Unique_Index
+    extends Dunagan_ProcessQueue_Block_Adminhtml_Index
+{
+    protected function _getTaskProcessorHelper()
+    {
+        return Mage::helper('dunagan_process_queue/task_processor_unique');
+    }
+}
